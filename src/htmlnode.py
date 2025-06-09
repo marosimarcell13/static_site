9e793bacf25b6d3a58d59e55a99da80e08700446
marosimarcell13 <marosimarcell13@gmail.com>
2025-06-09 11:43:36 +0200

    TextNode to html function added with unit tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -33,4 +33,23 @@ class LeafNode(HTMLNode):
 
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("invalid HTML: no tag")
+        elif self.children is None:
+            raise ValueError("invalid HTML: no children")
+        temp = ""
+        for child in self.children:
+            temp += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{temp}</{self.tag}>"
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
+            
+
             
