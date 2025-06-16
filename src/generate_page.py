f88586826feb2e6a0a8527fbbc20063509cfaaeb
marosimarcell13 <marosimarcell13@gmail.com>
2025-06-16 09:43:05 +0200

    Gitlab pages integration

--- src/generate_page.py
+++ src/generate_page.py
@@ -26,11 +26,11 @@ def generate_page(from_path, template_path, dest_path):
     with open(dest_path, "w") as file:
         file.write(result)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for filename in os.listdir(dir_path_content):
         dir_path = os.path.join(dir_path_content, filename)
         dest_path = os.path.join(dest_dir_path, filename)
-        if os.path.isfile(dir_path):
+        if os.path.isfile(dir_path):    
             file = open(dir_path,"r")
             temp_markdown = file.read()
             file.close()
@@ -42,12 +42,14 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
 
             with_title = temp_template.replace("{{ Title }}",title)
             result = with_title.replace("{{ Content }}", html)
+            result_1 = result.replace('href="/', f'href="{basepath}/')
+            result_2 = result_1.replace('src="/', f'src="{basepath}/')
 
             dest_dir_path1 = os.path.dirname(dest_path)
             if dest_dir_path1 != "":
                 os.makedirs(dest_dir_path1, exist_ok=True)
 
             with open(os.path.join(dest_dir_path1,"index.html"), "w") as file:
-                file.write(result)
+                file.write(result_2)
         else:
-            generate_pages_recursive(dir_path,template_path,dest_path)
+            generate_pages_recursive(dir_path,template_path,dest_path,basepath)
--- src/main.py
+++ src/main.py
@@ -1,24 +1,28 @@
 import os
 import shutil
+import sys
 
 from copy_static import copy_to_everything
 from generate_page import generate_pages_recursive
 
 dir_path_static = "./static"
 dir_path_public = "./public"
+dir_path_docs = "./docs"
 dir_path_template = "./template.html"
 dir_path_content = "./content"
 
+basepath = sys.argv[0]
+print(basepath)
 
 def main():
     print("Deleting public directory...")
-    if os.path.exists(dir_path_public):
-        shutil.rmtree(dir_path_public)
+    if os.path.exists(dir_path_docs):
+        shutil.rmtree(dir_path_docs)
     
-    copy_to_everything(dir_path_static,dir_path_public)
+    copy_to_everything(dir_path_static,dir_path_docs)
 
 
-    generate_pages_recursive(dir_path_content,dir_path_template,dir_path_public)
+    generate_pages_recursive(dir_path_content,dir_path_template,dir_path_docs,basepath)
 
 
 main()
